532e8fc126f93c1962512eb61322c0f67d5d368f
Jeffarese <jeff@wallapop.com>
2017-02-09 16:44:29 +0100

    Add toolbar component & theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from "@angular/material";
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ToolbarComponent
+	],
+	imports:      [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		MaterialModule.forRoot()
+	],
+	providers:    [],
+	bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
